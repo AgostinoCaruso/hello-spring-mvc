be6a213feda63651177496f96e28b6f0ed29024f
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-04-17 09:16:41 +0200

    la pagina home funziona

--- src/main/java/org/lessons/java/hello_spring_mvc/controller/homeController.java
+++ src/main/java/org/lessons/java/hello_spring_mvc/controller/homeController.java
@@ -2,13 +2,20 @@ package org.lessons.java.hello_spring_mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 @RequestMapping("/")
 public class homeController {
     
-    public String home(){
+    // public String getMethodName(@RequestParam String param) {
+        //     return new String();
+        // }
         
-        return("");
+    @GetMapping("/")
+    public String home(){
+
+        return("home");
     }
 }
